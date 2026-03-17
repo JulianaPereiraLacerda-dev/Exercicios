e967670ffb553d82dd845e16fa24ba23f1d911ad
JulianaPereiraLacerda-dev <julianapereiralacerda62@gmail.com>
2026-03-17 17:27:56 -0300

    :sparkles: feat: atividade inicial de sintaxe js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Livraria
+let cliente = "Juliana";
+let produto = 'Livro "O Pequeno Príncipe."';
+let PrecoUnitario = 50;
+let quantidade = 3;
+let TotaldaCompra = PrecoUnitario * quantidade;
+
+console.log("=========Livraria========");
+console.log(`Cliente: ${cliente}`);
+console.log(`Produto: ${produto}`);
+console.log(`Preço Unitário: R$${PrecoUnitario}`);
+console.log(`Quantidade: ${quantidade}`);
+console.log(`Total da Compra: ${TotaldaCompra} `);
+console.log("Pedido realizado com sucesso!");
+console.log("Obrigado pela preferência.");
+
+
